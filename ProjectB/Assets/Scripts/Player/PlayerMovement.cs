2deb5fe49e08676ffb41a228cee099e95d7a7c1f
Wessel <rigterwessel@gmail.com>
2023-02-03 21:48:33 +0100

    adds isgrounded check

--- ProjectB/Assets/Scripts/Player/PlayerMovement.cs
+++ ProjectB/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,24 +8,41 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
 
-
+    [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpForce;
-    private bool canJump = true;
+    private bool isGrounded = true;
+    public bool IsGrounded { get { return isGrounded; } }
+    Transform groundPoint;
     void Start(){
         rb = GetComponent<Rigidbody2D>();
+        groundPoint = transform.Find("Groundpoint");
+    }
+
+    private void Update(){
+        isGrounded = checkGrounded();
+        Debug.DrawLine(groundPoint.position, groundPoint.position + new Vector3(0,-0.25f,0), Color.red);
     }
     public void HandleMovement(InputAction.CallbackContext context){
         Debug.Log(context.ReadValue<float>());
     }
 
+
     public void Jump(InputAction.CallbackContext context){
         Debug.Log("jump");
-        if(!context.started || !canJump)
+        if(!context.started || !isGrounded)
             return;
 
         rb.AddForce(new Vector2(0, jumpForce));
 
-        canJump = false;
+        isGrounded = false;
 
+    }
+
+    /// <summary>
+    /// function that checks if player is grounded
+    /// </summary>
+    /// <returns>true if player collides with the ground</returns>
+    private bool checkGrounded(){
+        return Physics2D.CircleCast((Vector2) groundPoint.position, 0.25f, Vector2.zero, 0, LayerMask.GetMask("Ground"));
     }
 }
